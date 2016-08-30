4262052f5726f2391ff7b638d040c9ed56e5d6a2
xiaofanlinus <xiaofanlinus@gmail.com>
2016-08-29 20:51:57 -0700

    Finished My first homework

---
+++ hw1/Strings.java
@@ -0,0 +1,29 @@
+
+public class Strings {
+	public static void main(String[] args) {
+		String test = "harrasses";
+		String result = uniqueLetters(test);
+		System.out.println(result);
+	}
+	public static String uniqueLetters(String str)	{
+		int i;
+		int pos;
+		int length = str.length();
+		int[] ascii = new int[26];
+		String result = new String();
+		for(i = 0; i < length; i++) {
+			pos = str.charAt(i);
+			pos -= 97;
+			ascii[pos]++;
+		}
+			
+		for(i = 0; i < length; i++) {
+			pos = str.charAt(i);
+			pos -= 97;
+			if(ascii[pos] == 1) {
+				result += str.substring(i,i+1);
+			}
+		}
+		return result;
+	  }
+}
