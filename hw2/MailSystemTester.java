16e9ecbf5b750e6aae7c521ee4583b26691bd2d8
xiaofanlinus <xiaofanlinus@gmail.com>
2016-09-10 23:21:13 -0700

    Upload model files

---
+++ hw2/MailSystemTester.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+/**
+   This program tests the mail system. A single phone
+   communicates with the program through System.in/System.out.
+*/
+public class MailSystemTester
+{
+   public static void main(String[] args)
+   {
+      MailSystem system = new MailSystem(MAILBOX_COUNT);
+      Scanner console = new Scanner(System.in);
+      Console p = new Console(system, console);
+      p.run(c);
+   }
+
+   private static final int MAILBOX_COUNT = 20;
+}
