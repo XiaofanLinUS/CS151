5a0ae614aaf44ff7708d53d82171cc9e1b3546f5
xiaofanlinus <xiaofanlinus@gmail.com>
2016-12-16 13:32:46 -0800

    wip

--- final/problem6/DiamondNode.java
+++ final/problem6/DiamondNode.java
@@ -1,43 +1,85 @@
 import java.awt.Graphics2D;
+import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 
+@SuppressWarnings("serial")
 public class DiamondNode implements Node
 {
+
+   public DiamondNode(int Size)
+   {
+      this.size = Size;
+   }
 
    @Override
    public void draw(Graphics2D g2)
    {
-      Line2D.Double 
+      Line2D.Double l1 = new Line2D.Double(x + size / 2, y, x, y + size / 2);
+
+      Line2D.Double l2 = new Line2D.Double(x + size / 2, y, x + size / 2, y + size / 2);
+
+      Line2D.Double l3 = new Line2D.Double(x, y + size / 2, x + size / 2, y + size);
+
+      Line2D.Double l4 = new Line2D.Double(x + size / 2, y + size, x + size, y + size / 2);
+
+      g2.draw(l1);
+      g2.draw(l2);
+      g2.draw(l3);
+      g2.draw(l4);
 
    }
 
    @Override
    public void translate(double dx, double dy)
    {
-      // TODO Auto-generated method stub
+      x = x + dx;
+      y = y + dy;
 
    }
 
    @Override
    public boolean contains(Point2D aPoint)
    {
-      // TODO Auto-generated method stub
-      return false;
+
+      return getBounds().contains(aPoint);
    }
 
    @Override
-   public Point2D getConnectionPoint(Point2D aPoint)
+   public Point2D getConnectionPoint(Point2D other)
    {
-      // TODO Auto-generated method stub
+      double centerX = x + size / 2;
+      double centerY = y + size / 2;
+      double dx = other.getX() - centerX;
+      double dy = other.getY() - centerY;
+      double distance = Math.sqrt(dx * dx + dy * dy);
+      if (distance == 0)
+         return other;
+      else if (dx < dy && dx >= -dy)
+      {
+         return new Point2D.Double(x + size / 2, y);
+      } else if (dx >= dy && dx >= -dy)
+      {
+         return new Point2D.Double(x + size, y + size / 2);
+      } else if (dx >= -dy && dx < dy)
+      {
+         return new Point2D.Double(x, y + size / 2);
+      } else
+      {
+         return new Point2D.Double(x + size / 2, y + size);
+      }
+
       return null;
    }
 
    @Override
    public Rectangle2D getBounds()
    {
-      // TODO Auto-generated method stub
-      return null;
+      return new Rectangle2D.Double(x, y, size, size);
    }
+
+   private double x;
+   private double y;
+   private double size;
 
 }
--- final/problem6/SimpleGraph.java
+++ final/problem6/SimpleGraph.java
@@ -1,32 +1,19 @@
-import java.awt.*;
-import java.util.*;
+import java.awt.Color;
 
 /**
-   A simple graph with round nodes and straight edges.
-*/
+ * A simple graph with round nodes and straight edges.
+ */
 public class SimpleGraph extends Graph
 {
    public Node[] getNodePrototypes()
    {
-      Node[] nodeTypes =
-         {
-            new CircleNode(Color.BLACK),
-            new CircleNode(Color.WHITE)
-         };
+      Node[] nodeTypes = { new CircleNode(Color.BLACK), new CircleNode(Color.WHITE), new DiamondNode(100) };
       return nodeTypes;
    }
 
    public Edge[] getEdgePrototypes()
    {
-      Edge[] edgeTypes = 
-         {
-            new LineEdge()
-         };
+      Edge[] edgeTypes = { new LineEdge() };
       return edgeTypes;
    }
 }
-
-
-
-
-
