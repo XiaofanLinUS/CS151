5cb7a053a4de98c9b0d42e344a637f868c7bf9be
xiaofanlinus <xiaofanlinus@gmail.com>
2016-12-16 13:17:59 -0800

    wip

---
+++ final/problem6/DiamondNode.java
@@ -0,0 +1,43 @@
+import java.awt.Graphics2D;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
+
+public class DiamondNode implements Node
+{
+
+   @Override
+   public void draw(Graphics2D g2)
+   {
+      Line2D.Double 
+
+   }
+
+   @Override
+   public void translate(double dx, double dy)
+   {
+      // TODO Auto-generated method stub
+
+   }
+
+   @Override
+   public boolean contains(Point2D aPoint)
+   {
+      // TODO Auto-generated method stub
+      return false;
+   }
+
+   @Override
+   public Point2D getConnectionPoint(Point2D aPoint)
+   {
+      // TODO Auto-generated method stub
+      return null;
+   }
+
+   @Override
+   public Rectangle2D getBounds()
+   {
+      // TODO Auto-generated method stub
+      return null;
+   }
+
+}
